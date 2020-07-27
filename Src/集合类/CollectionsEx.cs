204eea9e5144fce4d256a2358bd8fd35a99a7fad
KaneLeung <kk.xiaolan@gmail.com>
2020-07-28 00:12:56 +0800

    :heavy_plus_sign:+增加集合转Csv字符串

--- Src/集合类/CollectionsEx.cs
+++ Src/集合类/CollectionsEx.cs
@@ -10,8 +10,8 @@
 * CLR 版本 ：4.0.30319.42000
 * 作　　者 ：Kane Leung
 * 创建时间 ：2020/05/05 15:27:09
-* 更新时间 ：2020/07/15 09:02:09
-* 版 本 号 ：v1.0.2.0
+* 更新时间 ：2020/07/27 09:02:09
+* 版 本 号 ：v1.0.3.0
 *******************************************************************
 * Copyright @ Kane Leung 2020. All rights reserved.
 *******************************************************************
@@ -22,6 +22,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace Kane.Extension
 {
@@ -203,5 +204,73 @@ namespace Kane.Extension
             else return false;
         }
         #endregion
+
+#if !NET40
+        #region 将集合转为Csv字符串 + ToCsv<T>(this IEnumerable<T> collection, bool hasTitle = true, string format = "yyyy-MM-dd HH:mm:ss")
+        /// <summary>
+        /// 将集合转为Csv字符串
+        /// </summary>
+        /// <typeparam name="T">集合类型</typeparam>
+        /// <param name="collection">当前集合</param>
+        /// <param name="hasTitle">是否包含标题列，即属性名</param>
+        /// <param name="format">时间转换的格式</param>
+        /// <returns></returns>
+        public static string ToCsv<T>(this IEnumerable<T> collection, bool hasTitle = true, string format = "yyyy-MM-dd HH:mm:ss")
+        {
+            var builder = new StringBuilder();
+            var props = typeof(T).GetProperties();
+            if (hasTitle)//包含标题
+            {
+                foreach (var item in props)
+                {
+                    builder.Append(item.Name);
+                    builder.Append(",");
+                }
+                builder.Append(Environment.NewLine);
+            }
+            foreach (var item in collection)
+            {
+                for (int i = 0; i < props.Length; i++)
+                {
+                    if (i != 0) builder.Append(",");
+                    var value = props[i].GetValue(item);
+                    if (value.IsNull()) builder.Append("\"\"");//如果为空，
+                    else
+                    {
+                        if (props[i].PropertyType == typeof(DateTime))//如果是时间格式，则检测格式中是否带有:半角的逗号【,】或半角的双引号【"】或换行符【\r\n】或【\n】,则字段必须放在引号内
+                        {
+                            builder.Append(ToCsvFormat(((DateTime)value).ToString(format)));
+                        }
+                        else if (props[i].PropertyType.IsGenericType && props[i].PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))//是否为可空类型
+                        {
+                            var temp = props[i].PropertyType.GenericTypeArguments.FirstOrDefault();
+                            if (temp == typeof(DateTime)) builder.Append(ToCsvFormat(((DateTime)value).ToString(format)));
+                            else if (temp == typeof(string)) builder.Append(ToCsvFormat((string)value));
+                            else builder.Append(value);
+                        }
+                        else if (props[i].PropertyType == typeof(string)) builder.Append(ToCsvFormat((string)value));
+                        else builder.Append(value);
+                    }
+                }
+                builder.Append(Environment.NewLine);
+            }
+            return builder.ToString();
+        }
+        #endregion
+
+        #region 判断字符串是否为特殊格式的字符串，如果是，则转为Csv格式的字符串 + ToCsvFormat(string value)
+        /// <summary>
+        /// 判断字符串是否为特殊格式的字符串，如果是，则转为Csv格式的字符串
+        /// </summary>
+        /// <param name="value">要判断字符串</param>
+        /// <returns></returns>
+        private static string ToCsvFormat(string value)
+        {
+            if (value.Contains("\"")) value = value.Replace("\"", "\"\"");
+            if (value.Contains("\"") || value.Contains(",") || value.Contains("\r\n") || value.Contains("\n")) value = $"\"{value}\"";
+            return value;
+        } 
+        #endregion
+#endif
     }
 }
